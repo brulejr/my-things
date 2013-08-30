253e4ee77cf1fe025b0a942418653b6d8f64e61f
Jon Brule <brulejr@gmail.com>
2013-08-29 21:44:02 -0400

    Integrated cluster control into app.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 // module dependencies
 var app = require("./app-init");
+var cluster = require('cluster');
+var control = require('strong-cluster-control');
 var http = require('http');
 
-// server instantiation
 var appname = app.locals.appname;
-var version = app.locals.version;
 var env = app.locals.env;
+var numCPUs = app.locals.config.numCPUs || require('os').cpus().length;
 var port = app.get('port');
-http.createServer(app).listen(port, function() {
-    console.log('%s v%s (%s) listening on port %d', appname, version, env, port);
-});
+var version = app.locals.version;
+
+if (cluster.isMaster) {
+
+	console.log('Starting %s v%s (s) - %d workers', appname, version, numCPUs);
+	control.start({
+		size: numCPUs
+	}).on('error', function(err){
+    console.error(err);
+	});
+
+} else {
+
+  // server instantiation
+  http.createServer(app).listen(port, function() {
+    console.log('Worker #%d listening on port %d', cluster.worker.id, port);
+  });
+
+}
